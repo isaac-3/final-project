6f57382e969bff701946f1841d789725381a5515
isaac-3 <isaacgc0596@gmail.com>
2020-07-11 20:15:31 -0500

    backend changes

--- backend/app/controllers/activity_reviews_controller.rb
+++ backend/app/controllers/activity_reviews_controller.rb
@@ -9,14 +9,11 @@ class ActivityReviewsController < ApplicationController
             activity_id: activity.id,
             user_id: user.id
         })
-        # byebug
         render json: review, :include => [:user]
     end
 
     def act_reviews
-        # byebug
         activity = Activity.find_or_create_by(name: params[:activity])
-        # byebug
         reviews = activity.activity_reviews
         render json: reviews, :include => [:user]
     end
